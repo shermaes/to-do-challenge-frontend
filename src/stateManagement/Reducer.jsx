698f10c99dabd74b98bfa4c4d9ba8253ead2010f
Sherilyn Maestre <you@example.com>
2022-05-04 14:55:56 -0500

    re-arranging the frontend based in components and state, added the reducer function

---
+++ src/stateManagement/Reducer.jsx
@@ -0,0 +1,11 @@
+function reducer(state, action){
+    switch(action.type){
+        case 'add-note':
+            return state
+        case 'remove-note':
+            return state
+        case 'update-note':        
+    }
+}
+
+export default reducer

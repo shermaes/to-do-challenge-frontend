3f0a1457a9b267f45176033a6d9ccfa205f10fd3
Sherilyn Maestre <you@example.com>
2022-05-04 17:31:27 -0500

    added the add-note feature

--- src/stateManagement/Reducer.jsx
+++ src/stateManagement/Reducer.jsx
@@ -1,7 +1,18 @@
 function reducer(state, action){
     switch(action.type){
         case 'add-note':
-            return state
+            const newNote = {
+                    id:Math.floor(Math.random()*100),
+                    message:action.payload.message,
+                    title:action.payload.title,
+                    category:action.payload.category,
+                    done:false
+            }
+            const newListOfNotesAddedOne = [...state.listOfNotes, newNote ]
+            const newStateAddNote = {
+                ...state, listOfNotes: newListOfNotesAddedOne
+            }
+            return newStateAddNote
         case 'remove-note':
             return state
         case 'update-note':        
